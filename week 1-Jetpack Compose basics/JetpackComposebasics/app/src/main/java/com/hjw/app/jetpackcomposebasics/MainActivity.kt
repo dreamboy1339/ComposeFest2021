2ee0b3de4a5fb2f12efa685ae07e909c49b5aa57
Jaewoo Hwang <dreamboy1339@gmail.com>
2021-11-07 22:18:45 +0900

    add row

--- week 1-Jetpack Compose basics/JetpackComposebasics/app/src/main/java/com/hjw/app/jetpackcomposebasics/MainActivity.kt
+++ week 1-Jetpack Compose basics/JetpackComposebasics/app/src/main/java/com/hjw/app/jetpackcomposebasics/MainActivity.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -40,9 +41,14 @@ fun Greeting(name: String) {
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
-        Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
-            Text(text = "Hello!")
-            Text(text = name)
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello!")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text("Show more")
+            }
         }
     }
 }
