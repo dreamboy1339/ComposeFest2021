48b3ba932e3279cfe084d1f3338676f4348f2595
Jaewoo Hwang <dreamboy1339@gmail.com>
2021-11-07 23:12:09 +0900

    Add animation to list

--- week 1-Jetpack Compose basics/JetpackComposebasics/app/src/main/java/com/hjw/app/jetpackcomposebasics/MainActivity.kt
+++ week 1-Jetpack Compose basics/JetpackComposebasics/app/src/main/java/com/hjw/app/jetpackcomposebasics/MainActivity.kt
@@ -3,6 +3,9 @@ package com.hjw.app.jetpackcomposebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -48,15 +51,21 @@ private fun MyApp() {
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember {
+    var expanded by remember {
         mutableStateOf(false)
     }
 
-    val extraPadding = if (expanded.value) {
-        48.dp
-    } else {
-        0.dp
-    }
+    val extraPadding by animateDpAsState(
+        if (expanded) {
+            48.dp
+        } else {
+            0.dp
+        },
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -66,14 +75,14 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello!")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+            OutlinedButton(onClick = { expanded = !expanded }) {
                 Text(
-                    if (expanded.value) {
+                    if (expanded) {
                         "Show less"
                     } else {
                         "Show more"
