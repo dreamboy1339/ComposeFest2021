d708199a826cfb296d8a51cdf46b612243b1b6a6
Jaewoo Hwang <dreamboy1339@gmail.com>
2021-11-07 22:38:39 +0900

    add state of compose

--- week 1-Jetpack Compose basics/JetpackComposebasics/app/src/main/java/com/hjw/app/jetpackcomposebasics/MainActivity.kt
+++ week 1-Jetpack Compose basics/JetpackComposebasics/app/src/main/java/com/hjw/app/jetpackcomposebasics/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,27 +29,47 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
+/*@Composable
 private fun MyApp() {
     // A surface container using the 'background' color from the theme
     Surface(color = MaterialTheme.colors.background) {
         Greeting("Android")
     }
-}
+}*/
 
 @Composable
 fun Greeting(name: String) {
+    val expanded = remember {
+        mutableStateOf(false)
+    }
+
+    val extraPadding = if (expanded.value) {
+        48.dp
+    } else {
+        0.dp
+    }
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello!")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text("Show more")
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(
+                    if (expanded.value) {
+                        "Show less"
+                    } else {
+                        "Show more"
+                    }
+                )
             }
         }
     }
