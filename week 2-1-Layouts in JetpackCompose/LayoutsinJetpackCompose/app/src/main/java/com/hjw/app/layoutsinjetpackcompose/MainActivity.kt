d97546d97a1ce3004012fc555a54d7b189aa1b1f
Jaewoo Hwang <dreamboy1339@gmail.com>
2021-11-14 23:31:53 +0900

    라운드 코너에서도 클릭 처리

--- week 2-1-Layouts in JetpackCompose/LayoutsinJetpackCompose/app/src/main/java/com/hjw/app/layoutsinjetpackcompose/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsinJetpackCompose/app/src/main/java/com/hjw/app/layoutsinjetpackcompose/MainActivity.kt
@@ -3,12 +3,14 @@ package com.hjw.app.layoutsinjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
@@ -18,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,6 +44,9 @@ class MainActivity : ComponentActivity() {
 fun PhotographerCard(modifier: Modifier = Modifier) {
     Row(
         modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
             .clickable(onClick = { /* Ignoring onClick */ })
             .padding(16.dp)
     ) {
