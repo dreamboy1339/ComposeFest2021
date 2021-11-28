6b68469687588d353113062b5c5069c7371b36d2
Jaewoo Hwang <dreamboy1339@gmail.com>
2021-11-28 16:51:37 +0900

    6. Displaying images: List Scrolling
    Layouts in Jetpack Compose 6.Working with lists

--- week 2-1-Layouts in JetpackCompose/LayoutsinJetpackCompose/app/src/main/java/com/hjw/app/layoutsinjetpackcompose/List.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsinJetpackCompose/app/src/main/java/com/hjw/app/layoutsinjetpackcompose/List.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -17,14 +18,41 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
+import com.hjw.app.layoutsinjetpackcompose.ui.theme.LayoutsInJetpackComposeTheme
 import kotlinx.coroutines.launch
 
 @Composable
-fun ImageListItem(index: Int) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
+fun LazyList() {
+    val scrollState = rememberLazyListState()
 
+    LazyColumn(state = scrollState) {
+        items(100) {
+            //Text("Item #$it")
+            ImageListItem(index = it)
+        }
+    }
+}
+
+@Composable
+fun SimpleList() {
+    val scrollState = rememberScrollState()
+
+    Column(Modifier.verticalScroll(scrollState)) {
+        repeat(100) {
+            Text("Item #$it")
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
         Image(
             painter = rememberImagePainter(
                 data = "https://developer.android.com/images/brand/Android_Robot.png"
@@ -34,6 +62,14 @@ fun ImageListItem(index: Int) {
         )
         Spacer(Modifier.width(10.dp))
         Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Preview
+@Composable
+fun ImageListItemPreview() {
+    LayoutsInJetpackComposeTheme {
+        ImageListItem(index = 0)
     }
 }
 
@@ -70,29 +106,6 @@ fun ScrollingList() {
             items(listSize) {
                 ImageListItem(it)
             }
-        }
-    }
-}
-
-@Composable
-fun LazyList() {
-    val scrollState = rememberLazyListState()
-
-    LazyColumn(state = scrollState) {
-        items(100) {
-            //Text("Item #$it")
-            ImageListItem(index = it)
-        }
-    }
-}
-
-@Composable
-fun SimpleList() {
-    val scrollState = rememberScrollState()
-
-    Column(Modifier.verticalScroll(scrollState)) {
-        repeat(100) {
-            Text("Item #$it")
         }
     }
 }
