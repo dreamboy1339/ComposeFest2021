1709157af0e1bf88519a7b4f950b41f62981036c
Jaewoo Hwang <dreamboy1339@gmail.com>
2021-11-28 16:35:56 +0900

    6. Displaying images
    Layouts in Jetpack Compose 6.Working with lists

--- week 2-1-Layouts in JetpackCompose/LayoutsinJetpackCompose/app/src/main/java/com/hjw/app/layoutsinjetpackcompose/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsinJetpackCompose/app/src/main/java/com/hjw/app/layoutsinjetpackcompose/MainActivity.kt
@@ -3,22 +3,10 @@ package com.hjw.app.layoutsinjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
@@ -27,13 +15,8 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.hjw.app.layoutsinjetpackcompose.ui.theme.LayoutsInJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -76,31 +59,7 @@ fun BodyContent(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         Text(text = "Hi there!")
         Text(text = "Thanks for going through the Layouts codelab")
-        LazyList()
-    }
-}
-
-@Composable
-fun LazyList() {
-    // We save the scrolling position with this state that can also
-    // be used to programmatically scroll the list
-    val scrollState = rememberLazyListState()
-
-    LazyColumn(state = scrollState) {
-        items(100) {
-            Text("Item #$it")
-        }
-    }
-}
-
-@Composable
-fun SimpleList() {
-    val scrollState = rememberScrollState()
-
-    Column(Modifier.verticalScroll(scrollState)) {
-        repeat(100) {
-            Text("Item #$it")
-        }
+        ScrollingList()
     }
 }
 
@@ -111,41 +70,3 @@ fun LayoutsCodelabPreview() {
         LayoutsCodelab()
     }
 }
-
-@Composable
-fun PhotographerCard(modifier: Modifier = Modifier) {
-    Row(
-        modifier
-            .padding(8.dp)
-            .clip(RoundedCornerShape(4.dp))
-            .background(MaterialTheme.colors.surface)
-            .clickable(onClick = { /* Ignoring onClick */ })
-            .padding(16.dp)
-    ) {
-        Surface(
-            modifier = Modifier.size(50.dp),
-            shape = CircleShape,
-            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
-        ) {
-            // Image goes here
-        }
-        Column(
-            modifier = Modifier
-                .padding(start = 8.dp)
-                .align(Alignment.CenterVertically)
-        ) {
-            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                Text("3 minutes ago", style = MaterialTheme.typography.body2)
-            }
-        }
-    }
-}
-
-//@Preview
-//@Composable
-//fun PhotographerCardPreview() {
-//    LayoutsInJetpackComposeTheme {
-//        PhotographerCard()
-//    }
-//}
